bf7355a0e7af1d1cbd51e627eeb57fd4c060906e
yayo <ofeist@gmail.com>
2023-03-24 15:13:03 +0100

    change rhel into centos

--- okd-setup/5_setup.sh
+++ okd-setup/5_setup.sh
@@ -7,7 +7,7 @@ sudo yum install wget -y
 
 # Part 2: Installing Docker
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
